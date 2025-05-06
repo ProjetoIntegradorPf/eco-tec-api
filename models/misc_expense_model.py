d0043387564ee2d29dca6483db154b0f916e4080
Djonatas Borges <interdjonatas@gmail.com>
2025-05-05 22:33:24 -0300

    add misc_expenses table

--- models/misc_expense_model.py
+++ models/misc_expense_model.py
@@ -4,14 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 import uuid
 import datetime
 
-Base = declarative_base()
-
+from database.database import Base
 class MiscExpenseModel(Base):
     __tablename__ = "misc_expenses"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     description = Column(String, nullable=False)
-    expense_date = Column(DateTime, default=datetime.datetime.utcnow)
+    expense_date = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
     value = Column(Float, nullable=False)
-    created_at = Column(DateTime, default=datetime.datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.datetime.utcnow, onupdate=datetime.datetime.utcnow)
+    created_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc))
+    updated_at = Column(DateTime, default=datetime.datetime.now(datetime.timezone.utc), onupdate=datetime.datetime.now(datetime.timezone.utc))
